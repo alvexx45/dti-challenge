74880d13e663aac71cf97a5d8f8b685e0af2400d
alvexx45 <bernardoalves222@gmail.com>
2025-11-25 17:48:52 -0300

    update api url

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
 
-const API_URL = process.env.REACT_APP_API_URL || 'https://dti-challenge.onrender.com';
+const API_URL = process.env.REACT_APP_API_URL || 'https://dti-challenge.onrender.com/';
 
 function App() {
   const [alunos, setAlunos] = useState([]);
