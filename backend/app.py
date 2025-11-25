4d7310f110d75e52e3a5d523aa30dede63737e58
alvexx45 <bernardoalves222@gmail.com>
2025-11-25 17:18:42 -0300

    try host through render

--- backend/app.py
+++ backend/app.py
@@ -2,6 +2,7 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 from typing import List, Dict
 import statistics
+import os
 
 app = Flask(__name__)
 CORS(app)
@@ -254,4 +255,5 @@ def home():
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=False, host='0.0.0.0', port=port)
